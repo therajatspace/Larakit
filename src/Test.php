dbcce5795013c1c961887b56ead88b5667388d64
Siddharth Sharma <siddharthsharmaofficial2@gmail.com>
2026-08-13 15:41:10 +0530

    setup package

---
+++ src/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sidd2604\larakit;
+
+class Test{
+    public function sayHello(){
+        return "Hello from larakit package";
+    }
+}
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Sidd2604\Larakit\Test;
+
+$test = new Test();
+
+echo $test->sayHello();
